28b8b4426f6570946b04a17dd638ce314d777e3b
kelvinsekx <utdkelvin@gmail.com>
2021-08-06 11:52:43 +0100

    wewewewew

--- server/webpack.production.js
+++ server/webpack.production.js
@@ -9,7 +9,7 @@ const config = {
   output: {
     path: path.join(cwd, "/dist/"),
     filename: "bundle.js",
-    publicPath: "https://competent-bhabha-9a4049.netlify.app/dist/",
+    publicPath: "/dist/",
   },
 	plugins: [new HtmlWebpackPlugin({template: "./public/index.html"})],
   module: {
