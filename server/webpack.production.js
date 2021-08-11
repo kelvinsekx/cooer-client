da294ff9d12dc20832e8b772c69ea03c5806dd35
kelvinsekx <utdkelvin@gmail.com>
2021-08-11 22:51:52 +0100

    .babel runtime itch

--- server/webpack.production.js
+++ server/webpack.production.js
@@ -25,12 +25,6 @@ const config = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              "@babel/preset-react"
-            ]
-          }
         },
       },
       { 
